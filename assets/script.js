17cf0c2c572bb1a40488ac9c7ace35c88443cc0e
TimothyYW <>
2023-10-18 14:31:01 +0000

    Add question for javascript

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const questions = [
+    {
+        question: "Which survivor got added during chapter Curtain call?",
+        a: "Dwight Fairfield",
+        b: "Kate Denson",
+        c: "Nea Karlson",
+        d: "Felix Richter",
+        correct: "b",
+    },
+    {
+        question: "Who have the power called spencer's last breath?",
+        a: "The doctor",
+        b: "The oni",
+        c: "The onryo",
+        d: "The nurse",
+        correct: "d",
+    }
+];
