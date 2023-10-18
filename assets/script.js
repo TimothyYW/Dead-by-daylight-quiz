deadc7dc2e99dd55bb38f68ad2b0ae18404bd3c5
TimothyYW <>
2023-10-18 22:53:42 +0000

    Add color for correct and incorrect

--- assets/script.js
+++ assets/script.js
@@ -49,7 +49,7 @@ function resetState() {
     while (answerButtons.firstChild)
 }
 
-function selectAnswer() {
+function selectAnswer(e) {
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if (isCorrect) {
