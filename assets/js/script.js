66c80e43e41592ad763a2de8beefa37d2230ae5b
TimothyYW <>
2023-10-21 13:01:16 +0000

    Add question up to ten

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,80 @@ const questions = [
             { text: "The onryo", correct: false },
             { text: "The nurse", correct: true },
         ]
+    },
+    {
+        question: "Whose survivor portrait is this?",
+        image: "assets/images/",
+        answers: [
+            { text: "Nicolas Cage", correct: true },
+            { text: "Nea Karlson", correct: false },
+            { text: "Claudette Morales", correct: false },
+            { text: "Felix Richter", correct: false },
+        ]
+    },
+    {
+        question: "Who is the fattest killer in Dead by daylight?",
+        answers: [
+            { text: "The Executioner", correct: false },
+            { text: "The Clown", correct: true },
+            { text: "The Legion", correct: false },
+            { text: "The Shape", correct: false },
+        ]
+    },
+    {
+        question: "Who came to the fog bringing the park called Boon:Circle of Healing?",
+        answers: [
+            { text: "David King", correct: false },
+            { text: "Nea Karlson", correct: false },
+            { text: "Zarina Kassier", correct: false },
+            { text: "Mikahela Reid", correct: true },
+        ]
+    },
+    {
+        question: "Which killer, whose real name is Susie?",
+        answers: [
+            { text: "The Legion", correct: true },
+            { text: "The Cenobite", correct: false },
+            { text: "The Oni", correct: false },
+            { text: "The Wraith", correct: false },
+        ]
+    },
+    {
+        question: "Whose killer portrait is this?",
+        image: "assets/images/",
+        answers: [
+            { text: "The Pleague", correct: false },
+            { text: "The Onryo", correct: false },
+            { text: "The Spirit", correct: true },
+            { text: "The Oni", correct: false },
+        ]
+    },
+    {
+        question: "Which survivor that have association with the killer name The Trickster?",
+        answers: [
+            { text: "Yumi Kimura", correct: false },
+            { text: "Jake Park", correct: false },
+            { text: "Yun-Jin-Lee", correct: true },
+            { text: "Feng Min", correct: false },
+        ]
+    },
+    {
+        question: "Who is the killer with the nickname The Skull Merchant",
+        answers: [
+            { text: "Herman Carter", correct: false },
+            { text: "Adriana Imai", correct: true },
+            { text: "Caleb Qinn", correct: false },
+            { text: "Amanda Young", correct: false },
+        ]
+    },
+    {
+        question: "Which survivor that have a Lebanese origin?",
+        answers: [
+            { text: "Zarina Kassier", correct: true },
+            { text: "Ada Wong", correct: false },
+            { text: "Meg Thomas", correct: false },
+            { text: "Gabriel Soma", correct: false },
+        ]
     }
 ];
 
